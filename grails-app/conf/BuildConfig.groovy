1c6ea77e0ef7475f9c0d062a9386d0e1e4ae8cc5
July Antonicheva <july.antonicheva@jcatalog.com>
2014-06-01 22:30:21 +0300

    Updated dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,10 +32,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        runtime ('mysql:mysql-connector-java:5.1.6',
-                 'org.springframework:spring-test:3.1.2.RELEASE')
-        compile ('com.sdicons.jsontools:jsontools-core:1.7')
-        compile ('commons-httpclient:commons-httpclient:3.0.1')
+        runtime ('mysql:mysql-connector-java:5.1.6')
     }
     
     plugins {
@@ -43,7 +40,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.47"
         runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.6"
 
-        runtime("org.grails.plugins:resources:1.1.6")
-        runtime('org.grails.plugins:mail:0.7.1')
+        runtime(":resources:1.2.8")
+        runtime(':mail:1.0.5')
     }
 }
