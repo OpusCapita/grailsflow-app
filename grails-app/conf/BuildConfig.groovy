e2e173a47110386c4e4a2ec7764c0334f01ae844
July Antonicheva <july.antonicheva@jcatalog.com>
2014-11-14 12:45:21 +0300

    (GFW-367) Prepared to release Grailsflow-1.4.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,14 +6,13 @@ grails.project.war.file = "${appName}-${appVersion}.war"
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-    inherits "global"
-    log "warn"
-
+    // inherit Grails' default dependencies
+    inherits( "global" ) {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-        inherits true
-        grailsPlugins()
-        grailsHome()
-
         if (grailsSettings.config.jcatalog.mavenLocal) {
             mavenLocal grailsSettings.config.jcatalog.mavenLocal
         }
@@ -21,12 +20,11 @@ grails.project.dependency.resolution = {
             mavenRepo grailsSettings.config.jcatalog.mavenCentral
         }
 
+        grailsPlugins()
+        grailsHome()
         grailsCentral()
         mavenCentral()
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        mavenRepo "http://repository.codehaus.org"
-        mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://repository.jboss.com/maven2/"
         mavenRepo "http://repo.grails.org/grails/core"
     }
 
@@ -48,9 +46,13 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.6"
 
         runtime ":resources:1.2.8"
-
         runtime ":mail:1.0.5"
 
-        compile ":grailsflow:1.4"
+        compile ":grailsflow:1.4.1"
+        runtime(':jquery:1.11.1',
+                ':jquery-ui:1.10.3',
+                ':jqplot:0.1')
+        compile ":export:1.6"
+        compile ":quartz:1.0.1"
     }
 }
